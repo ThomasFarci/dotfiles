0c7f60d24f44ed455311795b31554d19af2f8fe1
Thomas Real <ThomateReal@gmail.com>
2026-04-05 20:30:19 +0200

    Delete files.lua, I don't use the plugins.

--- nvim/.config/nvim/lua/plugins/files.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "stevearc/oil.nvim", opts = {} },
-  { "echasnovski/mini.files", opts = {} },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  },
-}
