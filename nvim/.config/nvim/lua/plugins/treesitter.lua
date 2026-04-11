d45d0b481d219addfdf44b892b3ec230a7139e32
Thomas Real <ThomateReal@gmail.com>
2026-04-11 16:32:40 +0200

    Add asciidoc parser for nvim-treesitter.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,4 +6,19 @@ table.insert(spec, {
   build = ":TSUpdate",
 })
 
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").asciidoc = {
+      install_info = {
+        url = "https://github.com/cathaysia/tree-sitter-asciidoc",
+        branch = "master",
+        location = "tree-sitter-asciidoc",
+        generate = true,
+        generate_from_json = true,
+      },
+    }
+  end,
+})
+
 return spec
