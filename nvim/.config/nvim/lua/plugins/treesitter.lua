cff62c02b789ef083585b393a1c4eebfb5eaabf7
Thomas Real <ThomateReal@gmail.com>
2026-04-14 16:45:37 +0200

    Add asccidoc inline parser.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -18,6 +18,15 @@ vim.api.nvim_create_autocmd("User", {
 				generate_from_json = true,
 			},
 		}
+		require("nvim-treesitter.parsers").asciidoc_inline = {
+			install_info = {
+				url = "https://github.com/cathaysia/tree-sitter-asciidoc",
+				branch = "master",
+				location = "tree-sitter-asciidoc_inline",
+				generate = true,
+				generate_from_json = true,
+			},
+		}
 	end,
 })
 
