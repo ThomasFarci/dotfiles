091aec9bc94a1c0b3813e76c7fa8611b6d7675bf
Thomas Real <ThomateReal@gmail.com>
2026-04-19 13:23:49 +0200

    Update kanata script to --filter=blob:none when cloning.

--- scripts/setup-kanata-mac.sh
+++ scripts/setup-kanata-mac.sh
@@ -9,37 +9,37 @@ You may press C-c at any point to exit.
 '
 
 if [[ "$BASH_SOURCE" != "$0" ]]; then
-  echo "run this file, don't source it."
-  return 0
+	echo "run this file, don't source it."
+	return 0
 fi
 
 cd "$(dirname "$BASH_SOURCE")"
 
 if [[ "$(command -v kanata)" == "" ]]; then
 
-  if [[ "$(command -v brew)" == "" ]]; then
-    if [[ "$(command -v cargo)" ]]; then
-      echo "Neither kanata nor brew nor cargo found."
-      exit
-    fi
-    echo "Kanata not found, installing with cargo..."
-    cargo install kanata
-  else
-    echo "Kanata not found, installing with brew..."
-    brew install kanata
-  fi
+	if [[ "$(command -v brew)" == "" ]]; then
+		if [[ "$(command -v cargo)" ]]; then
+			echo "Neither kanata nor brew nor cargo found."
+			exit
+		fi
+		echo "Kanata not found, installing with cargo..."
+		cargo install kanata
+	else
+		echo "Kanata not found, installing with brew..."
+		brew install kanata
+	fi
 
 fi
 
 if ! [[ -f /Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager ]]; then
-  echo "Karabiner vhid not found, cloning the repo temporarily..."
-  git clone https://github.com/pqrs-org/Karabiner-DriverKit-VirtualHIDDevice/ vhid-repo --depth 1
-  echo "Launching the karabiner virtual HID device installer, this may require your password."
-  open -W vhid-repo/dist/Karabiner-DriverKit-VirtualHIDDevice-6.9.0.pkg
-  echo "Deleting the vhid repo..."
-  command rm -rf vhid-repo
-  echo "When prompted (if prompted at all), please select the 'open settings' option and check 'Driver Extension'."
-  /Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
+	echo "Karabiner vhid not found, cloning the repo temporarily..."
+	git clone https://github.com/pqrs-org/Karabiner-DriverKit-VirtualHIDDevice/ vhid-repo --depth 1 --filter=blob:none
+	echo "Launching the karabiner virtual HID device installer, this may require your password."
+	open -W vhid-repo/dist/Karabiner-DriverKit-VirtualHIDDevice-6.9.0.pkg
+	echo "Deleting the vhid repo..."
+	command rm -rf vhid-repo
+	echo "When prompted (if prompted at all), please select the 'open settings' option and check 'Driver Extension'."
+	/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
 fi
 
 echo "Please add your kanata binary ($(command -v kanata)) to the list if you haven't already to grant it input monitoring permissions."
